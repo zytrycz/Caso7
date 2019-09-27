be0aabb88eeeb006d65e56269b9e1991aa5ba6e9
zytrycz <30675575+zytrycz@users.noreply.github.com>
2019-09-27 15:16:28 -0600

    Add Tuple, fixing main

---
+++ caso7/src/caso7/tuple.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caso7;
+
+/**
+ *
+ * @author brymo
+ */
+public class tuple {
+    private String value;
+    private int probability;
+
+    public tuple(String value, int probability) {
+        this.value = value;
+        this.probability = probability;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public int getProbability() {
+        return probability;
+    }
+    
+
+}
